42ded5fa7b412888cf46d425fad3bab2ffc633b0
jiadx <jiadx@yonyou.com>
2017-04-13 20:41:39 +0800

    restful

--- src/main/java/smart/factory/dao/CityDao.java
+++ src/main/java/smart/factory/dao/CityDao.java
@@ -1,15 +1,24 @@
-package smart.factory.dao.cluster;
+package smart.factory.dao;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 import smart.factory.domain.City;
 
+import java.util.List;
+
 /**
  * Created by jiadx on 17-4-13.
  */
-@Mapper
 @Repository
 public interface CityDao {
-    City findByName(@Param("cityName") String cityName);
+    List<City> findAllCity();
+
+    City findById(@Param("id") Long id);
+
+    Long saveCity(City city);
+
+    Long updateCity(City city);
+
+    Long deleteCity(Long id);
 }
---
+++ src/main/java/smart/factory/service/CityService.java
@@ -0,0 +1,50 @@
+package smart.factory.service;
+
+import smart.factory.domain.City;
+
+import java.util.List;
+
+/**
+ * Created by jiadx on 17-4-13.
+ */
+public interface CityService {
+
+    /**
+     * 获取城市信息列表
+     *
+     * @return
+     */
+    List<City> findAllCity();
+
+    /**
+     * 根据城市 ID,查询城市信息
+     *
+     * @param id
+     * @return
+     */
+    City findCityById(Long id);
+
+    /**
+     * 新增城市信息
+     *
+     * @param city
+     * @return
+     */
+    Long saveCity(City city);
+
+    /**
+     * 更新城市信息
+     *
+     * @param city
+     * @return
+     */
+    Long updateCity(City city);
+
+    /**
+     * 根据城市 ID,删除城市信息
+     *
+     * @param id
+     * @return
+     */
+    Long deleteCity(Long id);
+}
