1e9078a7d87939d884c51eae2532af4eb96e1885
jiadx <jiadx@yonyou.com>
2017-04-20 20:52:13 +0800

    mutli module

--- smart-factory-core/src/main/java/smart/factory/redis/RedisService.java
+++ smart-factory-core/src/main/java/smart/factory/redis/RedisService.java
@@ -1,5 +1,6 @@
 package smart.factory.redis;
 
+
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
--- smart-factory-web/src/test/java/smart/factory/SmartFactoryWebApplicationTests.java
+++ smart-factory-web/src/test/java/smart/factory/SmartFactoryWebApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SmartFactoryApplicationTests {
+public class SmartFactoryWebApplicationTests {
 
 	@Test
 	public void contextLoads() {
